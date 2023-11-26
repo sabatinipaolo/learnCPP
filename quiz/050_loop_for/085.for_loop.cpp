0b0d016f24cca874ea1ceb7da507562ff9bc6c89
paolo <paolo@foo.com>
2023-11-26 18:22:46 +0100

    Aggiunto quiz 050_loop_for/085...

---
+++ quiz/050_loop_for/085.for_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    for ( int c = 4, d = 0 ;  c >=  0 ; c-- , d++ )
+    {
+        cout << c << " " << d << endl;
+    } 
+        
+}
