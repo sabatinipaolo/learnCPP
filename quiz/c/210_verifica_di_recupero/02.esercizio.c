975129d80d72e4f4977bacdbfc117ff7bd48f616
sabatinipaolo <113015247+sabatinipaolo@users.noreply.github.com>
2024-01-18 19:53:43 +0100

    sistemata formattazione

--- quiz/c/210_verifica_di_recupero/02.esercizio.c
+++ quiz/c/210_verifica_di_recupero/02.esercizio.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void stampa_asterischi(int n){
-    
+void stampa_asterischi(int n)
+{
     for (int i= 1 ; i<= n ; i++)
         printf("*");
 }
 
-void rettangolo ( int numero_righe, int numero_colonne) {
-
+void rettangolo ( int numero_righe, int numero_colonne)
+{
     for (int i=1; i<= numero_righe; i++ )
     {
         stampa_asterischi(numero_colonne);
@@ -15,14 +15,13 @@ void rettangolo ( int numero_righe, int numero_colonne) {
     }
 }
 
-void forma ( int n ){
-    
+void forma ( int n )
+{
     rettangolo( n , n);
     rettangolo( n , 2*n );
 }
 
 int main()
 {
-
     forma(6);
 }
